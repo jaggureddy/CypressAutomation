23cc091a0d08b19220ca149371bad722a2881dc1
Jaggu <jagadeesh6010@gmail.com>
2020-06-06 18:08:06 -0400

    CYpress - 1

---
+++ cypress/integration/Scripts/TestScript.js
@@ -0,0 +1,10 @@
+describe('Simple test case', function(){
+    it('Firs test case', function(){
+        expect(true).to.equal(true)
+    })
+
+    it('Second test case',function(){
+        expect(false).to.equal(true)
+    })
+
+})
