78526b186ed15a149d21439b0f1e692e48dc76e6
JagguReddy <Jagadeesh6010@gmail.com>
2020-06-07 18:57:34 -0400

    Cypress 2

--- cypress/integration/Scripts/TestScript.js
+++ cypress/integration/Scripts/TestScript.js
@@ -1,10 +1,46 @@
+//    "baseUrl":"https://dry-ridge-67592.herokuapp.com/"
+//   "baseUrl":"https://example.cypress.io/"
+
+
 describe('Simple test case', function(){
-    it('Firs test case', function(){
-        expect(true).to.equal(true)
+    beforeEach(function(){
+        cy.visit('commands/querying')
     })
 
-    it('Second test case',function(){
-        expect(false).to.equal(true)
+    // before(function(){
+    //     cy.visit('commands/querying')
+    // })
+
+    it('Using Id', function(){
+        cy.get('#query-btn').should('contain', 'Button')
     })
+
+    it('Using class', () => {
+        cy.get('.query-btn').should('contain', 'Button')
+    })
+
+
+    it('Using CSS', () => {
+        cy.get('#querying .well>button:first').should('contain', 'Button')
+    })
+
+    it('Using Xpath', function(){
+        cy.xpath("//button[@id='query-btn']").should('contain', 'Button')
+    })
+
+    it('Using data attribute', () => {
+        cy.get('[data-test-id="test-example"]').should('have.class', 'example')
+    })
+
+    it('Invoke Attr method', function(){
+        cy.get('[data-test-id = "test-example"]').invoke('attr', 'data-test-id').should('equal','test-example')
+        cy.get('[data-test-id = "test-example"]').invoke('attr', 'class').should('equal','example')
+        cy.get('[data-test-id="test-example"]').invoke('css', 'position').should('equal', 'static')
+    })
+
+    it('Chaining assertions', () => {
+        cy.get('[data-test-id = "test-example"]').should('have.attr','class','example').and('have.css','position','static')
+    })
+
 
 })
